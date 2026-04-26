117fb513e368624b657d9c5d126883bb1502a573
Andrea Loderini <152980883+Andrealod96@users.noreply.github.com>
2026-04-26 13:27:13 +0200

    Delete fsersaservice directory

--- fsersaservice/application/port/in/RsaDocumentIngestionUseCase.java
+++
@@ -1,9 +0,0 @@
-package it.thcs.fse.fsersaservice.application.port.in;
-
-import it.thcs.fse.fsersaservice.api.dto.request.RsaDocumentIngestionRequest;
-import it.thcs.fse.fsersaservice.api.dto.response.RsaDocumentIngestionResponse;
-
-public interface RsaDocumentIngestionUseCase {
-
-    RsaDocumentIngestionResponse ingest(RsaDocumentIngestionRequest request);
-}
